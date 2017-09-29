a48e5a75aae104a3ff10fea7bc3ff015e3ee15db
pranay seela <pranayritvik@gmail.com>
2017-09-29 14:18:43 -0500

    Added windows form. U can select which file to scan

---
+++ TestWindowsForm/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "C:\\";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string filePath = openFileDialog1.FileName;
+                    var scanner = new AntiVirus.Scanner();
+                    var result = scanner.ScanAndClean(filePath);
+                    //var result = scanner.ScanAndClean(@"C:\test.txt");
+                    MessageBox.Show(result.ToString());
+                    //Console.WriteLine(result); // console output is "VirusNotFound".
+                    //Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+
+        }
+    }
+}
